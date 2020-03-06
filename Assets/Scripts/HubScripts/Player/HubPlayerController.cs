ef3b4fab820702c4f6cb9dc74f5d5c212ca7aab3
Nicholas Ng <nickng589@berkeley.edu>
2020-03-05 20:27:47 -0800

    added continuously moving player in the hubworld

---
+++ Assets/Scripts/HubScripts/Player/HubPlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HubPlayerController : MonoBehaviour
+{
+
+    Rigidbody2D playerRB;
+
+    float xAxis;
+    float yAxis;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRB = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        xAxis = Input.GetAxisRaw("Horizontal");
+        yAxis = Input.GetAxisRaw("Vertical");
+        Vector2 movementVector = new Vector2(xAxis, yAxis);
+        movementVector = movementVector * 4;
+        playerRB.velocity = movementVector;
+    }
+}
