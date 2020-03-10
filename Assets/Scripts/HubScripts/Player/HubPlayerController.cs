5b3b8c480608328e2a1edf11ff401ff692b60157
Nicholas Ng <nickng589@berkeley.edu>
2020-03-10 01:43:32 -0700

    basic dialouge done

--- Assets/Scripts/HubScripts/Player/HubPlayerController.cs
+++ Assets/Scripts/HubScripts/Player/HubPlayerController.cs
@@ -10,20 +10,29 @@ public class HubPlayerController : MonoBehaviour
     float xAxis;
     float yAxis;
 
+    public bool canMove;
+
     // Start is called before the first frame update
     void Start()
     {
+        canMove = true;
         playerRB = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        xAxis = Input.GetAxisRaw("Horizontal");
-        yAxis = Input.GetAxisRaw("Vertical");
-        Vector2 movementVector = new Vector2(xAxis, yAxis);
-        movementVector = movementVector * 4;
-        playerRB.velocity = movementVector;
+        if (canMove)
+        {
+            xAxis = Input.GetAxisRaw("Horizontal");
+            yAxis = Input.GetAxisRaw("Vertical");
+            Vector2 movementVector = new Vector2(xAxis, yAxis);
+            movementVector = movementVector * 4;
+            playerRB.velocity = movementVector;
+        } else
+        {
+            playerRB.velocity = Vector2.zero;
+        }
     }
 
 }
