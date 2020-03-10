b9384971cfa3ac25357ceee962a53012ee8139f5
Nicholas Ng <nickng589@berkeley.edu>
2020-03-10 02:49:16 -0700

    forgot to add new sprites

---
+++ Assets/Scripts/HubScripts/NPC/NPCBehavior.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCBehavior : MonoBehaviour
+{
+    #region Editor Variables
+    [SerializeField]
+    private NPCmanager npcmanager;
+
+    [SerializeField]
+    [Tooltip("The dialogue manager for the npc")]
+    DialogueManager dm;
+
+    [SerializeField]
+    [Tooltip("The player")]
+    HubPlayerController player;
+    #endregion
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") && Input.GetKeyDown("space") && player.canMove == true)
+        {
+            StartCoroutine("Example");
+        }
+    }
+
+    IEnumerator Example()
+    {
+        player.canMove = false;
+        dm.DisplayText(SceneDialogue.test);
+        yield return new WaitWhile(() => Input.anyKeyDown == false);
+        yield return new WaitWhile(() => !Input.GetKeyDown("space"));
+
+        dm.DisableTextBox();
+        dm.DisplayChoices(3, new string[] { "Press A for choice 1", "Press D for choice 2", "Press S for choice 3" });
+
+        yield return new WaitWhile(() => Input.anyKey == true);
+        yield return new WaitWhile(() => dm.GrabInput(3) == ChoiceSystem.Choices.Invalid);
+
+        dm.DisableChoices();
+
+        switch (dm.GrabInput(3))
+        {
+            case ChoiceSystem.Choices.Zero:
+                dm.DisplayText("You chose choice 1! Press space to close");
+                break;
+            case ChoiceSystem.Choices.One:
+                dm.DisplayText("You chose choice 2! Press space to close");
+                break;
+            case ChoiceSystem.Choices.Two:
+                dm.DisplayText("You chose choice 3! Press space to close");
+                break;
+        }
+
+        yield return new WaitWhile(() => Input.anyKeyDown == false);
+        yield return new WaitWhile(() => !Input.GetKeyDown("space"));
+
+        dm.DisableTextBox();
+        player.canMove = true;
+        yield return null;
+    }
+}
---
+++ Assets/Scripts/HubScripts/NPC/NPCmanager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCmanager : MonoBehaviour
+{
+	#region Editor Variables
+	[SerializeField]
+    [Tooltip("The NPCs in the hubworld")]
+    private NPCBehavior[] NPCs;
+
+    #endregion
+
+    #region
+	#endregion
+
+	// Start is called before the first frame update
+	void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
