977848fe3bff40e09ae633969d99c0e18bee9b83
Imaani Choudhuri <imaani@Imaanis-MacBook-Pro.local>
2020-04-28 01:43:06 -0700

    Finished hooking up HubWorld NPC animations

--- Assets/Scripts/HubScripts/NPC/NPCMovement.cs
+++ Assets/Scripts/HubScripts/NPC/NPCMovement.cs
@@ -5,24 +5,42 @@ using UnityEngine;
 public class NPCMovement : MonoBehaviour
 {
     public bool canMove;
+    public bool moveVertical;
 
     public bool walking;
-
-    public float walkTimer;
-
     public int ydir;
+    //public int ydist;
 
-    public int ydist;
+    public float timerMax;
+
+    [SerializeField]
+    private string tag_anim1;
+
+    [SerializeField]
+    private string tag_anim2;
+
+    [SerializeField]
+    private Animator anim;
+
+    private bool useAnim1;
+    private bool useAnim2;
+
+    private float walkTimer;
 
     Rigidbody2D npcRigidBody;
     // Start is called before the first frame update
     void Start()
     {
         ydir = -1;
+        if (timerMax == 0) {
+            timerMax = 1;
+        }
         walking = false;
         canMove = true;
-        walkTimer = 1;
+        walkTimer = timerMax;
         npcRigidBody = GetComponent<Rigidbody2D>();
+        useAnim1 = true;
+        useAnim2 = false;
     }
     void OnTriggerEnter2D(Collider2D other)
     {
@@ -46,26 +64,27 @@ public class NPCMovement : MonoBehaviour
         if (canMove == false)
         {
             npcRigidBody.velocity = Vector2.zero;
+            SetAnim(false, false);
         }
         else
         {
             walkTimer -= Time.deltaTime;
             if (walkTimer <= 0)
             {
-                Debug.Log("Start");
                 if (!walking)
                 {
-                    Debug.Log("first");
-                    //Vector2 movementVector = new Vector2(1 * ydir, 0);
-                    //npcRigidBody.velocity = movementVector;
-                    walkTimer = 1;
+                    //actually do movement
+                    SetAnim(false, false);
+                    Flip();
+                    walkTimer = timerMax;
                     walking = true;
                     ydir *= -1;
                 }
                 else if (walking)
                 {
-                    Debug.Log("second");
-                    walkTimer = 1;
+                    //Turn around, after timer elapses initiate above clause
+                    SetAnim(false, false);
+                    walkTimer = timerMax;
                     walking = false;
                     //npcRigidBody.velocity = Vector2.zero;
                 }
@@ -73,11 +92,30 @@ public class NPCMovement : MonoBehaviour
             if (walking)
             {
                 Vector2 movementVector = new Vector2(1 * ydir, 0);
+                if (moveVertical)
+                {
+                    movementVector = new Vector2(0, 1 * ydir);
+                }
+                
                 npcRigidBody.velocity = movementVector;
+                SetAnim(useAnim1, useAnim2);
             } else
             {
                 npcRigidBody.velocity = Vector2.zero;
             }
         }
+    }
+
+    void SetAnim(bool v1, bool v2) {
+        if (anim != null)
+        {
+            anim.SetBool(tag_anim1, v1);
+            anim.SetBool(tag_anim2, v2);
+        }
+    }
+
+    void Flip() {
+        useAnim1 = !useAnim1;
+        useAnim2 = !useAnim2;
     }
 }
